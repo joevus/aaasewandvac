b74a1e6dee3439a869fc01e77179690b3e0548eb
Joe Hos <joehoskisson@gmail.com>
2017-08-29 15:20:41 -0600

    changed to meet me on 33rd from utah screen prints

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-function initMap() {
-    var uluru = {lat: 40.702094, lng: -111.822953};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 14,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-}
+$(document).ready(function(){
+    var dateObj = new Date();
+    var dateYr = dateObj.getFullYear();
+    $("#currentYear").html(dateYr);
+});
